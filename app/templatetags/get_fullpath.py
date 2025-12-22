6b36534d81a866aa744b728924899139a68312e2
tomizawa0214 <j.tomizawa.japan@gmail.com>
2025-12-22 18:01:19 +0900

    【bugfix】staticファイルにキャッシュ更新のバージョンタグを付与

--- app/templatetags/get_fullpath.py
+++ app/templatetags/get_fullpath.py
@@ -1,4 +1,7 @@
+import os
+
 from django import template
+from django.contrib.staticfiles import finders
 from django.templatetags.static import static
 
 register = template.Library()
@@ -9,3 +12,17 @@ def get_fullpath(context, path):
     request = context.get("request")
     url = static(path)
     return request.build_absolute_uri(url) if request else url
+
+
+@register.simple_tag
+def static_version(path):
+    url = static(path)
+    full_path = finders.find(path)
+    if not full_path:
+        return url
+    try:
+        version = int(os.path.getmtime(full_path))
+    except OSError:
+        return url
+    joiner = "&" if "?" in url else "?"
+    return f"{url}{joiner}v={version}"
